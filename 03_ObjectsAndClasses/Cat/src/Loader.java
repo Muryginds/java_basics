2ce373bff56d94d6519d0592094c0fc60d536792
Muryginds <muryginds@gmail.com>
2020-12-21 10:21:14 +0300

    cats generated and while cycles added

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,8 +3,31 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat cat0 = new Cat();
+        System.out.println("Текущий вес кота0: " + cat0.getWeight());
 
-        System.out.println(cat.getStatus());
+        Cat cat1 = new Cat();
+        System.out.println("Текущий вес кота1: " + cat1.getWeight());
+
+        Cat cat2 = new Cat();
+        System.out.println("Текущий вес кота2: " + cat2.getWeight());
+
+        Cat cat3 = new Cat();
+        System.out.println("Текущий вес кота3: " + cat3.getWeight());
+
+        Cat cat4 = new Cat();
+        System.out.println("Текущий вес кота4: " + cat4.getWeight());
+
+        //Мяукаем до истощения
+        while (cat3.getStatus() != "Dead") {
+            cat3.meow();
+        }
+        System.out.println(cat3.getStatus());
+
+        //Взрываем кота
+        while (cat4.getStatus() != "Exploded") {
+            cat4.feed(1.0);
+        }
+        System.out.println(cat4.getStatus());
     }
 }
