5b19f8b150e833fca4b208bbb2ce037cbc2e22ee
Muryginds <muryginds@gmail.com>
2020-12-24 14:32:01 +0300

    4.1 String and Integer methods test

--- 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.1/IntegerExperiments/src/main/java/Main.java
@@ -4,23 +4,30 @@ public class Main {
     Container container = new Container();
     container.count += 7843;
 
-    int sum = sumDigits(7843);
+    Integer sum = sumDigits(7843);
 
     System.out.println(sum);
   }
 
-  /* Реализуйте метод sumDigits который возвращает сумму цифр числа, пример:
-  передано 12345, метод должен вернуть 15
-  если передано null, то должен вернуть -1
-
-  Запустите тесты TestSumDigits для проверки корректности работы метода
-
-  не меняйте название метода, его возвращаемое значение и модификаторы доступа (public).
-  В противном случае тестовый метод не сможет проверить ваш код
-   */
-
   public static int sumDigits(Integer number) {
-    //@TODO: write code here and delete TODO line
-    return 0;
+
+    int result = 0;
+
+    if (number == null){
+      result =  -1;
+    }
+    else {
+      String stringNumber = number.toString();
+      for (int i = 0; i < stringNumber.length(); i++) {
+        result = result + getNumberWithChar(stringNumber.charAt(i));
+      }
+    }
+    return result;
+  }
+
+  private static int getNumberWithChar(char symbol) {
+    String str = String.valueOf(symbol);
+    int num = Integer.parseInt(str);
+    return num;
   }
 }
