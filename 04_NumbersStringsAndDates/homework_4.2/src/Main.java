966c17d534e1071ad4cd6af0a7604c7d2859c875
Muryginds <muryginds@gmail.com>
2020-12-24 15:55:48 +0300

    4.2 numeric types

---
+++ 04_NumbersStringsAndDates/homework_4.2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+  public static void main(String[] args) {
+    System.out.println("Диапазон значений для Byte: " + Byte.MIN_VALUE + " + " + Byte.MAX_VALUE);
+    System.out.println("Диапазон значений для Short: " + Short.MIN_VALUE + " + " + Short.MAX_VALUE);
+    System.out.println("Диапазон значений для Integer: " + Integer.MIN_VALUE + " + " + Integer.MAX_VALUE);
+    System.out.println("Диапазон значений для Long: " + Long.MIN_VALUE + " + " + Long.MAX_VALUE);
+    System.out.println("Диапазон значений для Float: " + Float.MIN_VALUE + " + " + Float.MAX_VALUE);
+    System.out.println("Диапазон значений для Double: " + Double.MIN_VALUE + " + " + Double.MAX_VALUE);
+  }
+}
