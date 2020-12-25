d0cd35395b2fbce2a366972fd364ae4f5d64715d
Muryginds <muryginds@gmail.com>
2020-12-25 13:50:02 +0300

    4.4.2 calculateProfir method added

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,7 +3,20 @@ public class Main {
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
+
+    String lastPart = text.substring(text.lastIndexOf(",")+4, text.lastIndexOf(" руб"));
+
+    text = text.substring(0, text.lastIndexOf(","));
+    String middlePart = text.substring(text.lastIndexOf(",")+2, text.lastIndexOf(" руб"));
+
+    String firstPart = text.substring(0, text.indexOf(" руб"));
+
+    System.out.println("Сумма заработка всех друзей: ");
+    System.out.println(calculateProfit(firstPart) + calculateProfit(middlePart) + calculateProfit(lastPart));
   }
 
+  private static int calculateProfit(String string) {
+    String profit = string.substring(string.lastIndexOf(" "));
+    return Integer.parseInt(profit.trim());
+  }
 }
