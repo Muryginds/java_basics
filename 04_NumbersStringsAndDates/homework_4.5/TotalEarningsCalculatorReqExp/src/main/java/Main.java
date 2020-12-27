57492e9752ac213f9dd8561a34c50b9ac614f441
Muryginds <muryginds@gmail.com>
2020-12-27 20:51:05 +0300

    4.5.1 calculateSalarySum function added

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,21 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    System.out.println(calculateSalarySum(text));
   }
 
   public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
 
+    Pattern pattern = Pattern.compile("\\d+");
+    Matcher matcher = pattern.matcher(text);
+    int sum = 0;
+    while(matcher.find())
+      sum += Integer.parseInt(matcher.group());
+
+    return sum;
+  }
 }
