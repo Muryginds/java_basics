0db17a5da3543d0997eec14d61fbb01dbf078c12
Muryginds <muryginds@gmail.com>
2020-12-28 12:36:46 +0300

    4.5.1 constants added

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -3,19 +3,19 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+  private static final Pattern PATTERN = Pattern.compile("\\d+");
+
   public static void main(String[] args) {
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     System.out.println(calculateSalarySum(text));
   }
 
   public static int calculateSalarySum(String text){
-
-    Pattern pattern = Pattern.compile("\\d+");
-    Matcher matcher = pattern.matcher(text);
+    Matcher matcher = PATTERN.matcher(text);
     int sum = 0;
-    while(matcher.find())
+    while(matcher.find()){
       sum += Integer.parseInt(matcher.group());
-
+    }
     return sum;
   }
 }
