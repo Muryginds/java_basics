1266e74d743a3f88791fdf5abb57ff4fd2295820
Muryginds <muryginds@gmail.com>
2020-12-28 21:42:02 +0300

    4.5.4 phoneCleaner added

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+  private static final Pattern PATTERN_FOR_NORMALIZATION = Pattern.compile("\\s*(\\s|\\W| )\\s*");
+  private static final Pattern PATTERN_TO_CHECK_NUMBER = Pattern.compile("[7,8]{1}[0-9]{7,10}");
 
   public static void main(String[] args) {
 
@@ -10,8 +15,31 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+      checkStringIsValid(input);
     }
+
+    //checkStringIsValid("+7 909 123-45-67");
   }
 
+  private static void checkStringIsValid(String input) {
+    Matcher matcherForNormalization = PATTERN_FOR_NORMALIZATION.matcher(input);
+    String firstResult = matcherForNormalization.replaceAll("");
+
+    //System.out.println(firstResult);
+    int firstSymbol = firstResult.indexOf("8");
+
+    if ((firstResult.length() == 10)) {
+      firstResult = "7".concat(firstResult);
+    } else if ((firstResult.length() == 11 && firstSymbol == 0)) {
+      firstResult = "7".concat(firstResult.substring(1,firstResult.length()));
+    }
+
+    Matcher matcherCheckNumber = PATTERN_TO_CHECK_NUMBER.matcher(firstResult);
+
+    if (matcherCheckNumber.matches()) {
+      System.out.println(matcherCheckNumber.group());
+    } else {
+      System.out.println("Неверный формат номера");
+    }
+  }
 }
