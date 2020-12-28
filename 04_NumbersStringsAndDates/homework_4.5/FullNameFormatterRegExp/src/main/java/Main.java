bd4392670175bc622790332a7cecfe96d29a94e6
Muryginds <muryginds@gmail.com>
2020-12-28 15:02:13 +0300

    4.5.3 FullNameFormatter added

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+  private static final Pattern PATTERN_TO_CHECK_INPUT = Pattern.compile("(^[а-яА-Я-]{1,} [а-яА-Я-]{1,} [а-яА-Я-]{1,})$");
+  private static final Pattern PATTERN_TO_CORRECT_OUTPUT = Pattern.compile("\\s*(\\s)\\s*");
 
   public static void main(String[] args) {
 
@@ -10,9 +15,19 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      checkStringIsValid(input);
     }
   }
 
+  private static void checkStringIsValid(String input) {
+    Matcher matcherCheckInput = PATTERN_TO_CHECK_INPUT.matcher(input);
+    if (matcherCheckInput.find()) {
+      String[] names = PATTERN_TO_CORRECT_OUTPUT.split(matcherCheckInput.group(0));
+      System.out.println("Фамилия: " + names[0]);
+      System.out.println("Имя: " + names[1]);
+      System.out.println("Отчество: " + names[2]);
+    } else {
+      System.out.println("Введенная строка не является ФИО");
+    }
+  }
 }
