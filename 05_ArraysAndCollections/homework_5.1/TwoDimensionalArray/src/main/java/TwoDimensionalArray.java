491698f000091ae156d0d42630078c2de6690ecd
Muryginds <muryginds@gmail.com>
2021-01-06 17:09:24 +0300

    5.1.3 TwoDimensionalArray added

--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -1,14 +1,24 @@
 public class TwoDimensionalArray {
-    public static char symbol = 'X';
+    public static final char SYMBOL_X = 'X';
+    public static final char SYMBOL_SPACE = ' ';
 
     public static char[][] getTwoDimensionalArray(int size) {
 
-        //TODO: Написать метод, который создаст двумерный массив char заданного размера.
-        // массив должен содержать символ symbol по диагоналям, пример для size = 3
-        // [X,  , X]
-        // [ , X,  ]
-        // [X,  , X]
-
-        return new char[0][0];
+        char[][] array = new char[size][size];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (i == j) {
+                    array[i][i] = SYMBOL_X;
+                }
+                else if (array.length - i - 1 == j){
+                    array[i][j] = SYMBOL_X;
+                }
+                else {
+                    array[i][j] = SYMBOL_SPACE;
+                }
+            }
+            System.out.println(array[i]);
+        }
+        return array;
     }
 }
