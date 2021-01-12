736df8c6be340eaf471159343ef0aeda3dc3e94a
Muryginds <muryginds@gmail.com>
2021-01-12 16:07:07 +0300

    5.5 CoolNumbers homework

--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -5,25 +6,72 @@ import java.util.TreeSet;
 
 public class CoolNumbers {
 
+    public static final StringBuilder STRING_RESULT = new StringBuilder();
+    public static final String ALLOWED_LETTERS = "АВЕКМНОРСТУХ";
+    public static final int REGION_MAX = 199;
+    public static final int CARS_AMOUNT = 2000000;
+
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+
+        List<String> carNumbers = new ArrayList<>();
+
+        for (int i = 0; i < CARS_AMOUNT; i++) {
+            carNumbers.add(generateNumber());
+        }
+
+        return carNumbers;
+    }
+
+    private static String generateNumber() {
+        STRING_RESULT.setLength(0);
+        for (int i = 0; i < 7; i++){
+            if (i >= 1 && i < 4) {
+                STRING_RESULT.append(generateDigit());
+            } else if (i >= 4 && i < 6 || i == 0){
+                STRING_RESULT.append(generateLetter());
+            } else {
+                STRING_RESULT.append(generateRegion());
+            }
+        }
+        return STRING_RESULT.toString();
+    }
+
+    private static String generateRegion() {
+        String result = "" + (int)(Math.random()*(REGION_MAX - 1) + 1);
+        if (result.length() == 1) {
+            result = "0" + result;
+        }
+        return result;
+    }
+
+    private static int generateDigit() {
+        return (int)(Math.random()*9);
+    }
+
+    private static Character generateLetter() {
+        return ALLOWED_LETTERS.charAt((int)(Math.random()*(ALLOWED_LETTERS.length()-1)));
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        boolean searchIsSuccessful = false;
+        for(String element : list){
+           if (element.equals(number)){
+               searchIsSuccessful = true;
+           }
+        }
+        return searchIsSuccessful;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+       return Collections.binarySearch(sortedList, number) >= 0;
     }
 
-
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        return hashSet.contains(number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        return treeSet.contains(number);
     }
 
 }
--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
@@ -1,12 +1,46 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+
 public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
+
+    public static final String CAR_NUMBER = "М690ОВ116";
 
     public static void main(String[] args) {
+        List<String> generatedNumbers = CoolNumbers.generateCoolNumbers();
+        StringBuilder result = CoolNumbers.STRING_RESULT;
+        result.setLength(0);
+        boolean search = false;
+        String searchName = "";
+        for (int i = 0; i < 4; i++) {
+            Collections.sort(generatedNumbers);
+            HashSet hashSet = new HashSet<String>(generatedNumbers);
+            TreeSet treeSet = new TreeSet<String>(generatedNumbers);
+            long start = System.nanoTime();
+            switch (i){
+                case 0:
+                    search = CoolNumbers.bruteForceSearchInList(generatedNumbers, CAR_NUMBER);
+                    searchName = "Поиск перебором";
+                    break;
+                case 1:
+                    search = CoolNumbers.binarySearchInList(generatedNumbers, CAR_NUMBER);
+                    searchName = "Бинарный поиск";
+                    break;
+                case 2:
+                    search = CoolNumbers.searchInHashSet(hashSet, CAR_NUMBER);
+                    searchName = "Поиск в HashSet";
+                    break;
+                case 3:
+                    search = CoolNumbers.searchInTreeSet(treeSet, CAR_NUMBER);
+                    searchName = "Поиск в TreeSet";
+                    break;
+            }
+            long end = System.nanoTime();
+            result.append(searchName + ": номер "+ (search? "найден" : "не найден")
+                + ", поиск занял " + (end-start) + "нс\n");
+        }
 
+        System.out.println(result.toString());
     }
 }
