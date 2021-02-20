bfdf91d3936d4e252880d80bbfc28589f2b68fa1
Muryginds <muryginds@gmail.com>
2021-02-20 23:52:00 +0300

    7.2.1 emloyees lambda homework initialise

--- 07_AdvancedOOPFeatures/homework_7.2/Employees/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Employees/src/main/java/Main.java
@@ -1,18 +1,23 @@
+import java.time.ZoneId;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
     private static final String STAFF_TXT = "data/staff.txt";
 
     public static void main(String[] args) {
+
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
         System.out.println(employeeMaxSalary);
     }
 
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
-        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
-        // кто пришёл в году, указанном в переменной year
-        return null;
+
+        Optional<Employee> result = staff.stream().filter(e -> e.getWorkStart().toInstant().atZone(ZoneId.systemDefault())
+            .toLocalDate().getYear() == year).max(Comparator.comparingInt(Employee::getSalary));
+        return result.orElse(null);
     }
 }
