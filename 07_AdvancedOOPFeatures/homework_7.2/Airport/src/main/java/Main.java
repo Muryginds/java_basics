cf96e4e2fdb2100167760618d57f6978bd7ff181
Muryginds <muryginds@gmail.com>
2021-02-21 11:39:04 +0300

    7.2.2 stream api and lambda homework initialise

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,18 +1,30 @@
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
 
-import java.util.Collections;
+import com.skillbox.airport.Flight.Type;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+    Airport airport = Airport.getInstance();
+    List<Flight> flights = findPlanesLeavingInTheNextTwoHours(airport);
 
+    for (Flight flight : flights) {
+        System.out.println(flight);
+    }
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        return airport.getTerminals().stream().flatMap(t -> t.getFlights().stream()).filter(f -> {
+            LocalDateTime flightDate = f.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return f.getType() == Type.DEPARTURE && flightDate.isAfter(LocalDateTime.now()) && flightDate.isBefore(LocalDateTime.now().plusHours(2));
+        }).collect(Collectors.toList());
     }
 
 }
