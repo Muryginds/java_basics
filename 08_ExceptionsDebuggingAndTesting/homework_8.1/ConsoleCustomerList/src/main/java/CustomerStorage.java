2d9d2f10598384b9302df04b6b2f9a86cf7b3f54
Muryginds <muryginds@gmail.com>
2021-02-25 18:19:49 +0300

    8.1 exceptions homework initialisation

--- 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/homework_8.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,8 +1,12 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
+    private static final Pattern MAIL_PATTERN = Pattern.compile("\\S+\\@\\S+\\.\\S+");
+    private static final Pattern PHONE_PATTERN = Pattern.compile("\\+[7,8]{1}\\d{10}");
 
     public CustomerStorage() {
         storage = new HashMap<>();
@@ -15,6 +19,23 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong input format. Correct input example: \n"
+                + "\"add Василий Петров vasily.petrov@gmail.com +79215637722\"");
+        }
+        Matcher mailMatcher = MAIL_PATTERN.matcher(components[INDEX_EMAIL]);
+        Matcher phoneMatcher = PHONE_PATTERN.matcher(components[INDEX_PHONE]);
+
+        if (!mailMatcher.matches()){
+            throw new IllegalArgumentException("Wrong input format for e-mail. Correct input example: \n"
+                + "vasily.petrov@gmail.com");
+        }
+
+        if (!phoneMatcher.matches()){
+            throw new IllegalArgumentException("Wrong input format for phone number. Correct input example: \n"
+                + "+79215637722");
+        }
+
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
