2496dac5715c1a12c7c4f33407fdfa048fc2f7ee
Muryginds <muryginds@gmail.com>
2021-03-10 16:32:28 +0300

    9.1 homework with files initialise

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,6 +1,39 @@
+import java.io.File;
+
 public class FileUtils {
 
-    public static long calculateFolderSize(String path) {
+    public static long calculateFolderSize(String path)
+    {
+        try {
+            File file = new File(path);
+            return calculateAllElements(file);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         return 0;
+    }
+
+    private static long calculateAllElements(File file)
+    {
+        long result = 0;
+        if (file.isDirectory()) {
+            File[] files = null;
+
+            try {
+                files = file.listFiles();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            if (files == null || files.length == 0) {
+                return result;
+            }
+            for (File element : files) {
+                result += calculateAllElements(element);
+            }
+        } else {
+            result = file.length();
+        }
+        return result;
     }
 }
--- 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        long size = FileUtils.calculateFolderSize(input);
+        System.out.println(formatSize(size));
+    }
+
+    public static String formatSize(long v) {
+        if (v < 1024) return v + " B";
+        int z = (63 - Long.numberOfLeadingZeros(v)) / 10;
+        return String.format("%.1f %sB", (double)v / (Math.pow (2, z*10)), " KMGTPE".charAt(z));
     }
 }
