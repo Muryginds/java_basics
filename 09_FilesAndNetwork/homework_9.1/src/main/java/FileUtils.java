9d34f07e39c707b594751b6d88e5686894b28ce6
Muryginds <muryginds@gmail.com>
2021-03-11 13:40:35 +0300

    9.1 calculateFolderSize function reformated

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,38 +1,21 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class FileUtils {
 
     public static long calculateFolderSize(String path)
     {
-        try {
-            File file = new File(path);
-            return calculateAllElements(file);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return 0;
-    }
-
-    private static long calculateAllElements(File file)
-    {
         long result = 0;
-        if (file.isDirectory()) {
-            File[] files = null;
-
-            try {
-                files = file.listFiles();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-
-            if (files == null || files.length == 0) {
-                return result;
-            }
-            for (File element : files) {
-                result += calculateAllElements(element);
-            }
-        } else {
-            result = file.length();
+        try {
+            result =  Files.walk(Path.of(path))
+                .map(Path::toFile)
+                .filter(File::isFile)
+                .mapToLong(File::length)
+                .sum();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return result;
     }
