2d0db43b89512ef25c6e614a2afe3227769bea12
Muryginds <muryginds@gmail.com>
2021-03-11 13:41:55 +0300

    9.2 homework for folders copy initialised

--- 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
@@ -1,5 +1,46 @@
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        Path destination = Path.of(destinationDirectory);
+        Path source = Path.of(sourceDirectory);
+        try {
+            Files.walkFileTree(Path.of(sourceDirectory), new FileVisitor<>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException
+                {
+                    Path newd = destination.resolve(source.relativize(dir));
+                    Files.copy(dir, newd, StandardCopyOption.REPLACE_EXISTING);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
+                {
+                    Path newd = destination.resolve(source.relativize(file));
+                    Files.copy(file, newd, StandardCopyOption.REPLACE_EXISTING);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            System.out.println("Files copied successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String sourceDirectory = scanner.nextLine();
+        String destinationDirectory = scanner.nextLine();
+        FileUtils.copyFolder(sourceDirectory, destinationDirectory);
     }
 }
