4447b2ed69bf84486b92b90cfb90f132b76335b8
Muryginds <muryginds@gmail.com>
2021-03-31 09:17:38 +0300

    12.1 try-with-resources, constants added

--- 12_SQLAndHibernate/12.1_JDBC/src/main/java/Main.java
+++ 12_SQLAndHibernate/12.1_JDBC/src/main/java/Main.java
@@ -5,26 +5,22 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+  private static final String URL = "jdbc:mysql://localhost:3306/skillbox";
+  private static final String SQL_QUERY = "SELECT S.course_name, CAST(S.subs/M.months AS DECIMAL(16,2)) AS avg_subs FROM"
+      + "(SELECT course_name, COUNT(MONTH (subscription_date)) AS subs FROM PurchaseList GROUP BY course_name) S,"
+      + "(SELECT course_name, COUNT(sub_months) as months FROM (SELECT DISTINCT course_name, MONTH (subscription_date)"
+      + "AS sub_months FROM PurchaseList) X GROUP BY course_name) M "
+      + "WHERE S.course_name = M.course_name";
 
   public static void main(String[] args) {
-    String url = "jdbc:mysql://localhost:3306/skillbox";
-    String SQLQuery = "SELECT S.course_name, CAST(S.subs/M.months AS DECIMAL(16,2)) AS avg_subs FROM"
-        + "(SELECT course_name, COUNT(MONTH (subscription_date)) AS subs FROM PurchaseList GROUP BY course_name) S,"
-        + "(SELECT course_name, COUNT(sub_months) as months FROM (SELECT DISTINCT course_name, MONTH (subscription_date)"
-        + "AS sub_months FROM PurchaseList) X GROUP BY course_name) M "
-        + "WHERE S.course_name = M.course_name";
     String user = "root";
     String pass = "test";
-    try {
-      Connection connecton = DriverManager.getConnection(url, user, pass);
-      Statement statement = connecton.createStatement();
-      ResultSet resultSet = statement.executeQuery(SQLQuery);
+    try (Connection connecton = DriverManager.getConnection(URL, user, pass);
+        Statement statement = connecton.createStatement();
+        ResultSet resultSet = statement.executeQuery(SQL_QUERY)) {
       while (resultSet.next()) {
         System.out.println(resultSet.getString("course_name") + " - " + resultSet.getString("avg_subs"));
       }
-      resultSet.close();
-      statement.close();
-      connecton.close();
     } catch (SQLException ex) {
       ex.printStackTrace();
     }
