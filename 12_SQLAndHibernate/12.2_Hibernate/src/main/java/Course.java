24b88522fd0569e6d4f43b43009cafa5a992d5e4
Дмитрий Мурыгин <muryginds@gmail.com>
2021-04-14 18:45:12 +0000

    Revert "Revert "12.4 sql insertion + lombock initial""
    This reverts commit 7c34d4516c8b74ee89f0f0e290f84cce58b90506

--- 12_SQLAndHibernate/12.2_Hibernate/src/main/java/Course.java
+++ 12_SQLAndHibernate/12.2_Hibernate/src/main/java/Course.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,60 +13,40 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
 @Table(name = "Courses")
+@Data
 public class Course {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Getter
-  @Setter
   private int id;
 
-  @Getter
-  @Setter
   private String name;
 
-  @Getter
-  @Setter
   private int duration;
 
   @Enumerated(EnumType.STRING)
   @Column(columnDefinition = "enum", name = "type")
-  @Getter
-  @Setter
   private CourseType courseType;
 
-  @Getter
-  @Setter
   private String description;
 
-  @ManyToOne(cascade = CascadeType.ALL)
-  @Getter
-  @Setter
+  @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   private Teacher teacher;
 
   @Column(name = "students_count")
-  @Getter
-  @Setter
   private int studentsCount;
 
-  @Getter
-  @Setter
   private int price;
 
   @Column(name = "price_per_hour")
-  @Getter
-  @Setter
   private float pricePerHour;
 
   @ManyToMany(cascade = CascadeType.ALL)
   @JoinTable(name = "Subscriptions",
         joinColumns = {@JoinColumn(name = "course_id")},
         inverseJoinColumns = {@JoinColumn(name = "student_id")})
-  @Getter
-  @Setter
   List<Student> students;
 }
--- 12_SQLAndHibernate/12.2_Hibernate/src/main/java/Teacher.java
+++ 12_SQLAndHibernate/12.2_Hibernate/src/main/java/Teacher.java
@@ -3,24 +3,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
 @Table(name = "Teachers")
+@Data
 public class Teacher {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Getter
-  @Setter
   private int id;
-  @Getter
-  @Setter
+
   private int salary;
-  @Getter
-  @Setter
+
   private int age;
-  @Getter
-  @Setter
+
   private String name;
 }
